e308182aa96e249539ef62e4019c82c609eabd92
palash4263 <62851436+palash4263@users.noreply.github.com>
2021-10-11 16:27:06 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Input from "./Input";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+function App() {
+  return (
+    <div className="container"> {isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+  );
+}
+
+export default App;
